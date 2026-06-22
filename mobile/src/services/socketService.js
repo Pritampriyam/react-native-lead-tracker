a94c6d37b98acdfc567ac2bc0bdce1e2eeed8ad8
Pritam Kumar <pritampriyam07@gmail.com>
2026-06-22 11:08:27 +0530

    Emit lead from webhook

--- mobile/src/services/socketService.js
+++ mobile/src/services/socketService.js
@@ -1,7 +1,17 @@
 import { io } from "socket.io-client";
 
+console.log("Socket Service Loaded");
+
 const socket = io("https://react-native-lead-tracker.onrender.com", {
   transports: ["websocket"],
+});
+
+socket.on("connect", () => {
+  console.log("Socket Connected:", socket.id);
+});
+
+socket.on("connect_error", (err) => {
+  console.log("Socket Error:", err.message);
 });
 
 export default socket;
